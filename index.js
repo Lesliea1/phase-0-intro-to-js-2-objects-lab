9e183b8dd8e73c33d10a7e7980e15a833d83efd3
leslie <alvarado.leslie12@yahoo.com>
2023-03-09 15:56:46 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "leslie",
+    streetAddress: "222 N Fake St Phoenix Az 95037"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee =  {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value 
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = {employee};
+    employee[key] = `Sam`; 
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = {employee}
+    delete employee[key]
+   return employee;
+}
